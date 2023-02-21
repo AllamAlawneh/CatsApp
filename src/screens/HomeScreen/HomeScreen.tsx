f4a2ad7d6f6d17ca6dc8a036e2364da6932d9017
Hasan Alawneh <alawneh@zenhr.com>
2023-02-21 14:55:15 +0300

    cats list added

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,15 @@
-import {useFocusEffect} from '@react-navigation/native';
 import React, {useCallback, useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import {storageKeys} from '../../constants';
 import StorageHelper from '../../helpers/storage';
+import styles from './Styles';
+import {useFocusEffect} from '@react-navigation/native';
 
-function HomeScreen({navigation}) {
+interface HomeScreenProps {
+  navigation: any;
+}
+
+function HomeScreen({navigation}: HomeScreenProps) {
   const [cats, setCats] = useState<CatItem[]>([]);
 
   useFocusEffect(
@@ -19,20 +24,52 @@ function HomeScreen({navigation}) {
     const clonedCats = [...cats];
     clonedCats.splice(index, 1);
     setCats(clonedCats);
+
+    StorageHelper.saveItem(storageKeys.catsKey, clonedCats);
   };
 
+  const handleCatItemPress = () => {};
+
   const renderCatItem = ({item, index}: {item: CatItem; index: number}) => {
-    return null;
+    return (
+      <View style={styles.catLiContainer}>
+        <TouchableOpacity
+          style={styles.catLiLeftContainer}
+          activeOpacity={1}
+          onPress={handleCatItemPress}>
+          <Image
+            resizeMode="contain"
+            style={styles.imgStyle}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/616/616430.png',
+            }}
+          />
+
+          <Text style={styles.catLiName}>{'cat 1'}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => deleteCatItem(index)}>
+          <Image
+            resizeMode="contain"
+            style={styles.imgStyle}
+            source={{
+              uri: 'https://w1.pngwing.com/pngs/431/160/png-transparent-icon-design-trash-red-line-area-material-rectangle-thumbnail.png',
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
   };
 
   return (
-    <View style={{flex: 1, alignItems: 'center'}}>
-      <Text style={{color:"black"}}>Home screen</Text>
+    <View style={{flex: 1}}>
+      <Text style={{color: 'black'}}>Home screen</Text>
 
-      {/* <FlatList
+      <FlatList
         showsVerticalScrollIndicator={false}
         data={cats}
-        renderItem={renderCatItem} /> */}
+        renderItem={renderCatItem}
+      />
     </View>
   );
 }
--- src/screens/HomeScreen/Styles.ts
+++ src/screens/HomeScreen/Styles.ts
@@ -1,8 +1,32 @@
-import {StyleSheet} from 'react-native/types';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  catLiContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '90%',
+    alignSelf: 'center',
+    padding:16,
+    backgroundColor:'red',
+    marginBottom:16,
+    borderRadius:10
+  },
+  imgStyle: {
+    height: 25,
+    width: 25,
+  },
+  catLiName: {
+    marginLeft: 8,
+    color: 'blue',
+    fontSize: 16,
+  },
+  catLiLeftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
