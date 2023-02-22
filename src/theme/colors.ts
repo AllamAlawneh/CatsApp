16bf5d447b781f20dee0d376eb9069e6b37df6be
Raghad Abdelhadi <raghad@zenhr.com>
2023-02-22 14:19:15 +0300

    add color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,9 +1,9 @@
 const colors = {
-  primary: '#2d99ff',
+  primary: '#56c6ff',
   white: '#FFF',
   black: '#000',
   red: '#ff0e0e',
-  grey:'rgb(120,120,120)'
+  grey: 'rgb(120,120,120)',
 };
 
 export default colors;
